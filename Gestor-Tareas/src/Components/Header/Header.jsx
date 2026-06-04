fa9addcd82b1b68de4004b6b34ee28fd049e4d2c
AlissonJE <alissonjaramillo62@gmail.com>
2026-06-03 23:56:10 -0500

    Creacion de carpetas y base del proyecto/ Creacion de laestrutura del header y sus estilos

---
+++ Gestor-Tareas/src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { FaTasks } from "react-icons/fa";
+import './Header.css'
+
+export const Header = () => {
+    return(
+    <>
+     <header>
+        <FaTasks className="Logo" />
+        <h1>Gestor de Tareas</h1>
+     </header>
+        <hr />
+
+
+
+    </>
+    )
+}
